95457bdbe3e9fb4a2a9ca5089642127fe0af0705
Yanis Louvel <ylouvel@paul-f5Ar3s10.clusters.42paris.fr>
2025-02-15 19:45:42 +0100

    cpp02 finito

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,21 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed {
+public:
+    Fixed();
+    Fixed(const Fixed &src);
+    ~Fixed();
+
+    Fixed &operator=(const Fixed &rhs);
+
+    int getRawBits(void) const;
+    void setRawBits(int const raw);
+private:
+    int _value;
+    static const int _fractionalBits = 8;
+};
+
+#endif
---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,52 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() : _value(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &fixed) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = fixed;
+}
+
+Fixed::Fixed(const int value) {
+    std::cout << "Int constructor called" << std::endl;
+    _value = value << _fractionalBits;
+}
+
+Fixed::Fixed(const float value) {
+    std::cout << "Float constructor called" << std::endl;
+    _value = roundf(value * (1 << _fractionalBits));
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &fixed) {
+    std::cout << "Assignation operator called" << std::endl;
+    if (this != &fixed)
+        _value = fixed.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void) const {
+    return _value;
+}
+
+void Fixed::setRawBits(int const raw) {
+    _value = raw;
+}
+
+float Fixed::toFloat(void) const {
+    return (float)_value / (1 << _fractionalBits);
+}
+
+int Fixed::toInt(void) const {
+    return _value >> _fractionalBits;
+}
+
+std::ostream &operator<<(std::ostream &out, const Fixed &fixed) {
+    out << fixed.toFloat();
+    return out;
+}
